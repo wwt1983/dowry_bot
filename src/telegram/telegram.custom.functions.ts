9d64c5b67f7d7f34eaa716f661062c574828ab46
wwt1983 <wwt1983@yandex.ru>
2024-05-01 15:19:43 +0500

    change response for first step

--- src/telegram/telegram.custom.functions.ts
+++ src/telegram/telegram.custom.functions.ts
@@ -1,7 +1,12 @@
 import { CommandContext } from 'grammy';
 import { ITelegramWebApp, MyContext } from './telegram.interface';
 import { format } from 'date-fns';
-import { FIRST_STEP, HEADER } from './telegram.constants';
+import {
+  FIRST_STEP,
+  HEADER,
+  FIRST_STEP_A,
+  FIRST_STEP_B,
+} from './telegram.constants';
 export function sendMsgToSecretChat(ctx: CommandContext<MyContext>) {
   const { first_name, last_name, username, id } = ctx.from;
 
@@ -15,6 +20,8 @@ export function getTextForFirstStep(data: ITelegramWebApp): string {
     `Раздача: ${data.title} с кешбеком ${data.cash} рублей\n` +
     HEADER +
     FIRST_STEP +
-    data.keys
+    data.keys +
+    FIRST_STEP_A +
+    FIRST_STEP_B
   );
 }
