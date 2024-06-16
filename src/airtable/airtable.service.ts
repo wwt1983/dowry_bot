f4619f0c268b364278e45e74be5e507cd490ca4b
wwt1983 <wwt1983@yandex.ru>
2024-06-16 22:45:17 +0500

    fix timezone time table

--- src/airtable/airtable.service.ts
+++ src/airtable/airtable.service.ts
@@ -14,7 +14,7 @@ import { BotStatus } from './types/IBot.interface';
 import {
   FORMAT_DATE_SIMPLE,
   TIME_FULL,
-  dateFormat,
+  dateFormatNoTZ,
   getTimeWithTz,
 } from 'src/common/date/date.methods';
 import { ISessionData } from 'src/telegram/telegram.interface';
@@ -55,7 +55,7 @@ export class AirtableService {
       }
       const timeStartOffer =
         session.data.times && session.data.times?.length
-          ? ` (${session.data.times[2] === TIME_FULL ? dateFormat(session.data.times[0], FORMAT_DATE_SIMPLE) : session.data.times[0]})`
+          ? ` (${session.data.times[2] === TIME_FULL ? dateFormatNoTZ(session.data.times[0], FORMAT_DATE_SIMPLE) : session.data.times[0]})`
           : '';
 
       const data = {
