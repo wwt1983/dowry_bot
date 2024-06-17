1cee0c430ee3a64a5eac2c15a768ce71aebed6d7
wwt1983 <wwt1983@yandex.ru>
2024-06-17 15:58:26 +0500

    limit to 60

--- src/telegram/telegram.constants.ts
+++ src/telegram/telegram.constants.ts
@@ -11,7 +11,7 @@ export const FILE_FROM_BOT_URL = 'https://api.telegram.org/file/bot';
 export const WEB_APP = 'https://dowrybot-front.vercel.app';
 export const WEB_APP_TEST = 'https://dowry-bot.netlify.app/';
 export const STOP_TEXT = 'Раздачу продолжать нельзя';
-export const LIMIT_TIME_IN_MINUTES_FOR_ORDER = 30;
+export const LIMIT_TIME_IN_MINUTES_FOR_ORDER = 60;
 export const START_NAME = '👉 Dowry раздачи 👈';
 
 export enum COMMAND_NAMES {
