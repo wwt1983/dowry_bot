e05524c5cbe323258e78fbdcc598dad3ce5c6f6d
wwt1983 <wwt1983@yandex.ru>
2024-07-11 03:59:41 +0500

    try fix

--- src/telegram/puppeteer/index.ts
+++ src/telegram/puppeteer/index.ts
@@ -2,7 +2,9 @@ import puppeteer from 'puppeteer';
 
 export const getParseWbInfo = async (articul: string) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.goto(
       `https://www.wildberries.ru/catalog/${articul}/detail.aspx`,
