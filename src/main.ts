e79b794d70a017ae07001422df250caf07768fab
wwt1983 <wwt1983@yandex.ru>
2024-08-19 13:21:02 +0500

    add sertificate

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./src/cert/private.pem'),
+  cert: fs.readFileSync('./src/cert/cert.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST'],
